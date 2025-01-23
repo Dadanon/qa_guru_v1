7ee4efa6a1f854dd49de1ecaf6b3ca275fbc0052
Mar4ello <mar4ello19922@gmail.com>
2025-01-23 09:37:23 +0300

    Оформлен README, сделаны все тесты

--- tests/test_authors.py
+++ tests/test_authors.py
@@ -3,7 +3,7 @@ from pydantic import ValidationError
 from starlette import status
 from starlette.testclient import TestClient
 
-from schemas import AuthorDetail
+from schemas import AuthorDetail, AuthorUpdate, AuthorCreate
 
 
 def test_get_authors(new_author, client: TestClient):
@@ -48,3 +48,82 @@ def test_get_author(new_author, new_book, client: TestClient):
     assert len(data['books']) == 2
     assert data['books'][0] == book_1.title
     assert data['books'][1] == book_2.title
+
+
+def test_get_author_books(new_author, new_book, client: TestClient):
+    """
+    Тестируем получение подробной информации о книгах автора на тестовой базе
+    Создаём автора, пару книг для него и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Super', 'Man')
+    book_1 = new_book('About all', 15.3, author_1.id)
+    book_2 = new_book('Nothing to do', 173.2, author_1.id)
+
+    # Act
+    response = client.get(f'/api/authors/{author_1.id}/books')
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert data['total'] == 2
+    assert len(data['books']) == 2
+    assert data['books'][0]['title'] == book_1.title
+    assert data['books'][1]['title'] == book_2.title
+
+
+def test_update_author(new_author, new_book, client: TestClient):
+    """
+    Тестируем изменение данных автора на тестовой базе
+    Создаём автора, пару книг для него, изменяем данные автора и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Super', 'Man')
+    new_book('About all', 15.3, author_1.id)
+    new_book('Nothing to do', 173.2, author_1.id)
+    author_update_form: AuthorUpdate = AuthorUpdate(first_name='Mega')
+
+    # Act
+    response = client.put(f'/api/authors/{author_1.id}', json=author_update_form.model_dump())
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert data['id'] == author_1.id
+    assert data['full_name'] == 'Mega Man'  # INFO: не очень литералами проверять, но пока лучше не придумал
+    assert len(data['books']) == 2
+
+
+def test_create_author(new_author, client: TestClient):
+    """
+    Тестируем создание автора на тестовой базе
+    Создаём автора и проверяем инфу
+    """
+    # Arrange
+    author_create_form: AuthorCreate = AuthorCreate(first_name='Mega', last_name='Man')
+
+    # Act
+    response = client.post(f'/api/authors', json=author_create_form.model_dump())
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_201_CREATED
+    assert data['full_name'] == f'{author_create_form.first_name} {author_create_form.last_name}'
+    assert len(data['books']) == 0
+
+
+def test_delete_author(new_author, client: TestClient):
+    """
+    Тестируем удаление автора на тестовой базе
+    Создаём автора, удаляем его и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Mega', 'Man')
+
+    # Act
+    response = client.delete(f'/api/authors/{author_1.id}')
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert data == author_1.id
--- tests/test_books.py
+++ tests/test_books.py
@@ -1,6 +1,8 @@
 from starlette import status
 from starlette.testclient import TestClient
 
+from schemas import BookUpdate, BookCreate
+
 
 def test_get_books(new_author, new_book, client: TestClient):
     """
@@ -24,3 +26,83 @@ def test_get_books(new_author, new_book, client: TestClient):
     assert data[1]['id'] == book_2.id
     assert data[0]['title'] == book_1.title
     assert data[1]['title'] == book_2.title
+
+
+def test_get_book(new_author, new_book, client: TestClient):
+    """
+    Тестируем получение подробной информации о книге на тестовой базе
+    Создаём автора, книгу для него и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Super', 'Man')
+    book_1 = new_book('About all', 15.3, author_1.id)
+
+    # Act
+    response = client.get(f'/api/books/{book_1.id}')
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert data['id'] == book_1.id
+    assert data['price'] == book_1.price
+    assert data['author_name'] == author_1.full_name
+
+
+def test_update_book(new_author, new_book, client: TestClient):
+    """
+    Тестируем изменение данных книги на тестовой базе
+    Создаём автора, книгу для него, изменяем данные книги и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Super', 'Man')
+    book_1 = new_book('About all', 15.3, author_1.id)
+    book_update_form: BookUpdate = BookUpdate(title='Nothing to say', price=22.2)
+
+    # Act
+    response = client.put(f'/api/books/{book_1.id}', json=book_update_form.model_dump())
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert data['id'] == book_1.id
+    assert data['title'] == book_update_form.title
+    assert data['price'] == book_update_form.price
+    assert data['author_name'] == author_1.full_name
+
+
+def test_create_book(new_author, new_book, client: TestClient):
+    """
+    Тестируем создание книги у имеющегося автора на тестовой базе
+    Создаём автора, книгу и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Super', 'Man')
+    book_create_form: BookCreate = BookCreate(title='About all', price=15.3, author_id=author_1.id)
+
+    # Act
+    response = client.post(f'/api/books', json=book_create_form.model_dump())
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_201_CREATED
+    assert data['title'] == book_create_form.title
+    assert data['price'] == book_create_form.price
+    assert data['author_name'] == author_1.full_name
+
+
+def test_delete_book(new_author, new_book, client: TestClient):
+    """
+    Тестируем удаление книги на тестовой базе
+    Создаём автора, книгу для него, удаляем книгу и проверяем инфу
+    """
+    # Arrange
+    author_1 = new_author('Mega', 'Man')
+    book_1 = new_book('About all', 15.3, author_1.id)
+
+    # Act
+    response = client.delete(f'/api/books/{book_1.id}')
+    data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert data == book_1.id
